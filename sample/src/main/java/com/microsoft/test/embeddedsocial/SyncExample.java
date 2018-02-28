b6dc715f4ad307e1da1cede050f1d1856176ae9d
Stefan Saroiu <ssaroiu@microsoft.com>
2018-02-28 13:39:27 -0800

    simple http client and retrofit

--- sample/src/main/java/com/microsoft/test/embeddedsocial/SyncExample.java
+++ sample/src/main/java/com/microsoft/test/embeddedsocial/SyncExample.java
@@ -1,5 +1,11 @@
 package com.microsoft.test.embeddedsocial;
 
+import android.text.method.ReplacementTransformationMethod;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by ssaroiu on 2/28/2018.
  *
@@ -7,6 +13,20 @@ package com.microsoft.test.embeddedsocial;
  */
 
 public class SyncExample {
+
+    public OkHttpClient.Builder httpClient;
+    public Retrofit retrofit;
+
+    // Initializes an http client and retrofit
+    public SyncExample(String ESUrl)
+    {
+        httpClient = new OkHttpClient.Builder();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(ESUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+    }
 
     public  void run() {
         System.out.println("Stefan rules.");
