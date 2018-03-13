06f7eafc5e45a33411c1d1bba9910f0b44b9cc14
Stefan Saroiu <ssaroiu@microsoft.com>
2018-03-12 18:10:14 -0700

    very rough draft of batching implemented

--- sample/src/main/java/com/microsoft/test/embeddedsocial/BatchedExample.java
+++ sample/src/main/java/com/microsoft/test/embeddedsocial/BatchedExample.java
@@ -5,26 +5,40 @@ import com.microsoft.embeddedsocial.autorest.EmbeddedSocialBatchedClientImpl;
 import com.microsoft.embeddedsocial.autorest.EmbeddedSocialClientImpl;
 import com.microsoft.embeddedsocial.autorest.models.BuildsCurrentResponse;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+
 /**
  * Created by ssaroiu on 3/6/2018.
  */
 
 public class BatchedExample {
-    private EmbeddedSocialClientImpl esClient;
+    private EmbeddedSocialBatchedClientImpl esBatchClient;
+    private final int k;
 
     // Initializes an ES client that supports batching
-    public BatchedExample(String ESUrl)
+    public BatchedExample(String ESUrl, int k)
     {
-        EmbeddedSocialBatchedClientImpl.Builder esBatchedClientBuilder = new EmbeddedSocialBatchedClientImpl.Builder().baseUrl(ESUrl);
-        this.esClient = esBatchedClientBuilder.build().getEsClient();
+        this.k = k;
+        EmbeddedSocialBatchedClientImpl.Builder esBatchedClientBuilder = new EmbeddedSocialBatchedClientImpl.Builder().baseUrl(ESUrl).batchSize(k);
+        this.esBatchClient = esBatchedClientBuilder.build();
     }
 
     // Batches k requests into a single call to Embedded Social server
-    public void run(int k) {
+    public void run() {
         for (int i = 0; i < k; i += 1) {
-            AsyncExample asyncExample = new AsyncExample(this.esClient);
+            AsyncExample asyncExample = new AsyncExample(this.esBatchClient.getEsClient());
             asyncExample.run();
         }
-    }
 
+        while (!this.esBatchClient.isBatchReady());
+
+        try {
+            this.esBatchClient.issueBatch();
+        }
+        catch (Exception e) {
+            System.err.println("Batched call failed with exception: " +  e.getMessage());
+        }
+    }
 }
