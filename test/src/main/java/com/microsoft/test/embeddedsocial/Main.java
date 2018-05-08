bed0b8747c687f33789faa87ab18ef1b0880aeb9
Stefan Saroiu <ssaroiu@microsoft.com>
2018-05-08 13:16:13 -0700

    added new test around users, topics, comments

--- test/src/main/java/com/microsoft/test/embeddedsocial/Main.java
+++ test/src/main/java/com/microsoft/test/embeddedsocial/Main.java
@@ -13,18 +13,19 @@ public class Main {
     // Here's a short set of instructions on how to obtain an access token from MSA:
     // 1) Put this URL in browser:
     // https://login.live.com/oauth20_authorize.srf?client_id=0000000040169587&scope=wl.signin%20wl.basic&response_type=token&redirect_uri=http://research.microsoft.com
+    // Follow the login steps with MSA account (note that you can use your Hotmail/Outlook/Google e-mail address)
     // 2) Redirects to RMC - copy the redirected URL in MyURL constant below ...
     // Will look like this:
     // https://www.microsoft.com/en-us/research/#access_token=_long_access_token_&token_type=bearer&expires_in=3600&..._more_more_..."
     // Extract the access token by hand from the string above and create an auth string that looks like:
     // "Microsoft AK=_your_app_key|TK=_access_token_"
-    private final static String auth = "_FILL_WITH_AUTH_";
+    private final static String auth = "Microsoft AK=_your_app_key|TK=_your_token_";
 
     public static void main(String[] args) {
         SyncTest syncTest = new SyncTest(esUrl);
         AsyncTest asyncTest = new AsyncTest(esUrl);
         BatchTest batchTest = new BatchTest(esUrl, 1);
-        PostUserAsyncTest postUserAsyncTest = new PostUserAsyncTest(esUrl, auth);
+        UserTopicCommentAsyncTest userTopicCommentAsyncTest = new UserTopicCommentAsyncTest(esUrl, auth);
 
         // syncTest makes single synchronous call
         syncTest.run();
@@ -36,6 +37,29 @@ public class Main {
         batchTest.run();
 
         // All calls made from this point request a proper auth
-        postUserAsyncTest.run();
+        userTopicCommentAsyncTest.postUser();
+
+        // Wait 1 seconds for the call to complete
+        SleepOneSecond();
+
+        userTopicCommentAsyncTest.postTopic();
+        SleepOneSecond();
+        userTopicCommentAsyncTest.postComment();
+        SleepOneSecond();
+        userTopicCommentAsyncTest.deleteComment();
+        SleepOneSecond();
+        userTopicCommentAsyncTest.deleteTopic();
+        SleepOneSecond();
+        userTopicCommentAsyncTest.deleteUser();
     }
+
+    public static void SleepOneSecond() {
+        try {
+            Thread.sleep(1 * 1000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
