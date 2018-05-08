7e5f3a183f97980211717883ebd83751e19569ce
Stefan Saroiu <ssaroiu@microsoft.com>
2018-05-08 11:54:10 -0700

    adding instructions

--- test/src/main/java/com/microsoft/test/embeddedsocial/Main.java
+++ test/src/main/java/com/microsoft/test/embeddedsocial/Main.java
@@ -8,6 +8,16 @@ package com.microsoft.test.embeddedsocial;
 public class Main {
     // Url to Embedded Social instance (use PPE for this example)
     private final static String esUrl = "https://ppe.embeddedsocial.microsoft.com";
+
+    // ES accepts access tokens from Facebook, MSA, Google, and Twitter.
+    // Here's a short set of instructions on how to obtain an access token from MSA:
+    // 1) Put this URL in browser:
+    // https://login.live.com/oauth20_authorize.srf?client_id=0000000040169587&scope=wl.signin%20wl.basic&response_type=token&redirect_uri=http://research.microsoft.com
+    // 2) Redirects to RMC - copy the redirected URL in MyURL constant below ...
+    // Will look like this:
+    // https://www.microsoft.com/en-us/research/#access_token=_long_access_token_&token_type=bearer&expires_in=3600&..._more_more_..."
+    // Extract the access token by hand from the string above and create an auth string that looks like:
+    // "Microsoft AK=_your_app_key|TK=_access_token_"
     private final static String auth = "_FILL_WITH_AUTH_";
 
     public static void main(String[] args) {
